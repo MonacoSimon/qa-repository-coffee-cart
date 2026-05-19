498ceead03f92011366a6495a9a5e1e4005fc4ac
simon <simonmonaco86@gmail.com>
2026-05-19 17:31:22 -0300

    cambio direccion del host

--- cloud-testing/aws/s3/upload_reports.py
+++ cloud-testing/aws/s3/upload_reports.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 s3_client = boto3.client(
     's3',
-    endpoint_url='http://localhost:4566',
+    endpoint_url='http://host.docker.internal:4566',
     aws_access_key_id='test',
     aws_secret_access_key='test',
     region_name='us-east-1'
